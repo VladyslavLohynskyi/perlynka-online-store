44a646227a04bd012ce83313990b252ad85db38e
VladyslavLohynskyi <watcher.good2003@gmail.com>
2022-11-24 23:16:06 +0200

    server: added adding sizes of shoes when creating

--- server/src/controllers/shoesController.ts
+++ server/src/controllers/shoesController.ts
@@ -2,6 +2,12 @@ import Shoes from '../models/shoesModel';
 import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
 import { Request, Response } from 'express';
+import ShoesSize from '../models/shoesSizeModel';
+
+interface IParseSizes {
+   sizeId: number;
+   count: number;
+}
 
 interface shoesCreateRequest extends Request {
    body: {
@@ -11,13 +17,15 @@ interface shoesCreateRequest extends Request {
       colorId: number;
       seasonId: number;
       brandId: number;
+      sizes: string;
    };
 }
 
 class shoesController {
    async create(req: shoesCreateRequest, res: Response) {
       try {
-         const { model, price, brandId, typeId, colorId, seasonId } = req.body;
+         const { model, price, brandId, typeId, colorId, seasonId, sizes } =
+            req.body;
          const img = req.files?.file;
          if (!img) {
             return res.json('Error: Upload one file');
@@ -29,7 +37,7 @@ class shoesController {
             res.json('Error: Upload only one file');
          }
 
-         const device = await Shoes.create({
+         const shoes = await Shoes.create({
             model,
             price,
             rating: 0,
@@ -39,7 +47,11 @@ class shoesController {
             seasonId,
             img: fileName,
          });
-         return res.json(device);
+         const parseSizes: IParseSizes[] = JSON.parse(sizes);
+         parseSizes.map(({ sizeId, count }) =>
+            ShoesSize.create({ sizeId, count, shoId: shoes.id }),
+         );
+         return res.json(shoes);
       } catch (error) {
          res.json('Shoes creating Error ');
       }
