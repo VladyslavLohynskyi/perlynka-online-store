3f7ff623bed8d667bd2391208ca792f609f791ba
VladyslavLohynskyi <watcher.good2003@gmail.com>
2023-12-09 23:07:54 +0200

    added image and basic info to shoes page

--- client/src/modules/shoes/pages/ShoesPage.tsx
+++ client/src/modules/shoes/pages/ShoesPage.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import './ShoesPage.scss';
 import { useParams } from 'react-router-dom';
 import { IParticularShoes, getShoesById } from '../../../http/shoes';
+import { baseURL } from '../../../utils/constants';
 
 export const ShoesPage: React.FC = () => {
    const { id } = useParams();
@@ -12,10 +13,28 @@ export const ShoesPage: React.FC = () => {
    }, []);
    return (
       <div className='shoes-page__container'>
-         {currentShoes?.id}
-         {currentShoes?.brand.name}
-         {currentShoes?.model}
-         {currentShoes?.price}
+         {currentShoes && (
+            <div className='shoes-page__main'>
+               <div className='shoes-page__img-container'>
+                  <img src={baseURL + currentShoes.img} alt='Взуття' />
+               </div>
+               <div className='shoes-page__info-container'>
+                  <h3 className='shoes-page__model-name'>
+                     {currentShoes.brand.name} {currentShoes.model}
+                  </h3>
+                  <p className='shoes-page__model-id'>
+                     ID товару: #{currentShoes.id}
+                  </p>
+                  <p className='shoes-page__model-price'>
+                     Ціна: {currentShoes.price}грн
+                  </p>
+                  <p className='shoes-page__model-size'>Розмір взуття:</p>
+                  <div className='shoes-page__sizes-buttons'></div>
+                  <div className='shoes-page__buy-container'></div>
+                  <p>Відгуки:0</p>
+               </div>
+            </div>
+         )}
       </div>
    );
 };
