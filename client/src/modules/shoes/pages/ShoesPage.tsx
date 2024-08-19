ea30260313d28dceba90c4936969d2fc30c92f00
VladyslavLohynskyi <watcher.good2003@gmail.com>
2024-08-19 17:27:01 +0300

    added LoaderType component interface, added loader to ShoesPage

--- client/src/modules/shoes/pages/ShoesPage.tsx
+++ client/src/modules/shoes/pages/ShoesPage.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import './ShoesPage.scss';
-import { useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import ShoesReq, { IParticularShoes, IShoesImage } from '../../../http/shoes';
 import { HorizontalLine } from '../../ui/HorizontalLine';
 import ShoesCarousel from '../components/ShoesCarousel/ShoesCarousel';
 import ShoesInfos from '../components/ShoesInfos/ShoesInfos';
 import { Descriptions } from '../components/Descriptions';
+import { Loader } from '../../ui/Loader';
+import { faL } from '@fortawesome/free-solid-svg-icons';
+import { RoutesEnum } from '../../../utils/constants';
 
 export enum BuyButtonTextEnum {
    BUY = 'Купити',
@@ -13,20 +16,32 @@ export enum BuyButtonTextEnum {
 }
 
 export const ShoesPage: React.FC = () => {
+   const navigate = useNavigate();
    const { id } = useParams();
    const [currentShoes, setCurrentShoes] = useState<IParticularShoes>();
+   const [isLoading, setIsLoading] = useState<boolean>(false);
    const [slides, setSlides] = useState<IShoesImage[]>([]);
    useEffect(() => {
-      if (id)
-         ShoesReq.getShoesById(+id).then((shoes) => {
-            setCurrentShoes(shoes);
-            setSlides([
-               { id: 0, img: shoes.img, shoId: shoes.id },
-               ...shoes.shoes_images,
-            ]);
-         });
+      if (id) {
+         setIsLoading(true);
+         ShoesReq.getShoesById(+id)
+            .then((shoes) => {
+               setCurrentShoes(shoes);
+               setSlides([
+                  { id: 0, img: shoes.img, shoId: shoes.id },
+                  ...shoes.shoes_images,
+               ]);
+            })
+            .catch(() => {
+               navigate(RoutesEnum.SHOP);
+            })
+            .finally(() => setIsLoading(false));
+      }
    }, []);
 
+   if (isLoading) {
+      return <Loader className='shoes-page__loader' />;
+   }
    return (
       <div className='shoes-page'>
          {currentShoes && (
--- client/src/modules/ui/Loader/Loader.tsx
+++ client/src/modules/ui/Loader/Loader.tsx
@@ -1,10 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import './Loader.scss';
+import { LoaderType } from './LoaderType';
 
-export const Loader: React.FC = () => {
+export const Loader: React.FC<LoaderType> = ({ className, ...props }) => {
    return (
-      <div className='loader__container'>
+      <div className={'loader__container ' + className} {...props}>
          <FontAwesomeIcon icon={faSpinner} size='3x' spin={true} />
       </div>
    );
