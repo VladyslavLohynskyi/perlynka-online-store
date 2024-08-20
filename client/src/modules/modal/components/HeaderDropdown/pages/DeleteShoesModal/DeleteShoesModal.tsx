b05d5b05523193669b83c02a153730d28eb3a5f5
VladyslavLohynskyi <watcher.good2003@gmail.com>
2024-08-20 20:22:48 +0300

    added loader to DeleteShoesModal and EditShoesModal

--- client/src/modules/modal/components/HeaderDropdown/pages/DeleteShoesModal/DeleteShoesModal.tsx
+++ client/src/modules/modal/components/HeaderDropdown/pages/DeleteShoesModal/DeleteShoesModal.tsx
@@ -11,6 +11,7 @@ import { useAppDispatch, useAppSelector } from '../../../../../../hooks/redux';
 import { ModalSearch } from '../../components/ModalSearch/';
 import { ButtonClassEnum } from '../../../../../ui/Button/ButtonType';
 import { HorizontalLine } from '../../../../../ui/HorizontalLine';
+import { Loader } from '../../../../../ui/Loader';
 export const DeleteShoesModal: React.FC<DeleteShoesModalType> = ({
    onClose,
 }) => {
@@ -20,13 +21,16 @@ export const DeleteShoesModal: React.FC<DeleteShoesModalType> = ({
    const [id, setId] = useState<number>(0);
    const [error, setError] = useState('');
    const [foundShoes, setFoundShoes] = useState<null | IShoes>(null);
+   const [isShoesLoading, setIsShoesLoading] = useState<boolean>(false);
    const dispatch = useAppDispatch();
    const handleSubmitId = async () => {
       setError('');
       setFoundShoes(null);
+      setIsShoesLoading(true);
       ShoesReq.getShoesById(id)
          .then((data) => setFoundShoes(data))
-         .catch((e) => setError(e.response.data));
+         .catch((e) => setError(e.response.data))
+         .finally(() => setIsShoesLoading(false));
    };
 
    const handleChangeId = (newId: string) => {
@@ -36,77 +40,86 @@ export const DeleteShoesModal: React.FC<DeleteShoesModalType> = ({
       <div className='delete-shoes-modal__container  modal-container'>
          <ModalHeader text='Видалити взуття' onClose={onClose} />
          <div className='edit-shoes-modal__main'>
-            <ModalSearch
-               label='Індефікатор'
-               value={id}
-               setValue={handleChangeId}
-               handleSubmitValue={handleSubmitId}
-               type={'number'}
-               text='Введіть ID'
-            />
-            {error && <p className='modal__error'>{error}</p>}
-            <HorizontalLine />
-            {foundShoes && (
-               <div className='delete-shoes-modal__shoes-view'>
-                  <div className='delete-shoes-modal__shoes-view-container'>
-                     <div className='delete-shoes-modal__shoes-view-img-container'>
-                        <img
-                           style={{ width: '100%' }}
-                           src={baseURL + foundShoes.img + '.webp'}
-                           alt='Взуття'
-                        />
-                     </div>
+            {isShoesLoading ? (
+               <Loader />
+            ) : (
+               <>
+                  <ModalSearch
+                     label='Індефікатор'
+                     value={id}
+                     setValue={handleChangeId}
+                     handleSubmitValue={handleSubmitId}
+                     type={'number'}
+                     text='Введіть ID'
+                  />
+                  {error && <p className='modal__error'>{error}</p>}
+                  <HorizontalLine />
+                  {foundShoes && (
+                     <div className='delete-shoes-modal__shoes-view'>
+                        <div className='delete-shoes-modal__shoes-view-container'>
+                           <div className='delete-shoes-modal__shoes-view-img-container'>
+                              <img
+                                 style={{ width: '100%' }}
+                                 src={baseURL + foundShoes.img + '.webp'}
+                                 alt='Взуття'
+                              />
+                           </div>
 
-                     <div className='delete-shoes-modal___shoes-view-info-container'>
-                        <p>Модель: {foundShoes.model}</p>
-                        <p>Ціна: {foundShoes.price}</p>
-                        <p>Стать: {foundShoes.sex}</p>
-                        <p>
-                           Бренд:{' '}
-                           {
-                              brands?.find(
-                                 (brand) => foundShoes.brandId === +brand.id,
-                              )?.name
-                           }
-                        </p>
-                        <p>
-                           Тип:{' '}
-                           {
-                              types?.find(
-                                 (type) => foundShoes.typeId === +type.id,
-                              )?.name
-                           }
-                        </p>
-                        <p>
-                           Сезон:{' '}
-                           {
-                              seasons?.find(
-                                 (season) => foundShoes.seasonId === +season.id,
-                              )?.name
-                           }
-                        </p>
-                        <p>
-                           Колір:{' '}
-                           {
-                              colors?.find(
-                                 (color) => foundShoes.colorId === +color.id,
-                              )?.name
-                           }
-                        </p>
+                           <div className='delete-shoes-modal___shoes-view-info-container'>
+                              <p>Модель: {foundShoes.model}</p>
+                              <p>Ціна: {foundShoes.price}</p>
+                              <p>Стать: {foundShoes.sex}</p>
+                              <p>
+                                 Бренд:{' '}
+                                 {
+                                    brands?.find(
+                                       (brand) =>
+                                          foundShoes.brandId === +brand.id,
+                                    )?.name
+                                 }
+                              </p>
+                              <p>
+                                 Тип:{' '}
+                                 {
+                                    types?.find(
+                                       (type) => foundShoes.typeId === +type.id,
+                                    )?.name
+                                 }
+                              </p>
+                              <p>
+                                 Сезон:{' '}
+                                 {
+                                    seasons?.find(
+                                       (season) =>
+                                          foundShoes.seasonId === +season.id,
+                                    )?.name
+                                 }
+                              </p>
+                              <p>
+                                 Колір:{' '}
+                                 {
+                                    colors?.find(
+                                       (color) =>
+                                          foundShoes.colorId === +color.id,
+                                    )?.name
+                                 }
+                              </p>
+                           </div>
+                        </div>
+                        <div>
+                           <Button
+                              disabled={foundShoes ? false : true}
+                              buttonClass={ButtonClassEnum.DELETE}
+                              buttonText='Видалити'
+                              buttonClick={() => {
+                                 dispatch(deleteShoes(id));
+                                 onClose();
+                              }}
+                           />
+                        </div>
                      </div>
-                  </div>
-                  <div>
-                     <Button
-                        disabled={foundShoes ? false : true}
-                        buttonClass={ButtonClassEnum.DELETE}
-                        buttonText='Видалити'
-                        buttonClick={() => {
-                           dispatch(deleteShoes(id));
-                           onClose();
-                        }}
-                     />
-                  </div>
-               </div>
+                  )}
+               </>
             )}
          </div>
       </div>
