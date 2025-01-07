24f6f541fe35eabb1209309b0ecba0a681682dd1
VladyslavLohynskyi <watcher.good2003@gmail.com>
2025-01-07 04:31:12 +0200

    clear: remove unneccessary code fpr event to send change page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,7 +8,6 @@ import { APIProvider } from '@vis.gl/react-google-maps';
 
 import ReactGA from 'react-ga4';
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALITICS_KEY!);
-ReactGA.send('pageview');
 const store = setupStore();
 const root = ReactDOM.createRoot(
    document.getElementById('root') as HTMLElement,
